7f91d914a4d0f4af9a006cfee9a9aad9e1a97785
Maheboob7699 <murdimaheboob@gmail.com>
2025-02-28 01:44:34 -0900

    Fixed json-server issue

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 import jsonServer from "json-server";
+import cors from "cors";  // Ensure CORS is included
+
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
-const port = 5000;
 
+server.use(cors());  // Use CORS middleware
 server.use(middlewares);
 server.use(router);
 
+const port = process.env.PORT || 5000;
 server.listen(port, () => {
   console.log(`🚀 JSON Server is running at http://localhost:${port}`);
 });

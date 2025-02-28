aeaa5e38101b3a58305ad3c8fb435a342632e6ba
Maheboob7699 <murdimaheboob@gmail.com>
2025-02-28 01:24:51 -0900

    start json server

---
+++ index.js
@@ -0,0 +1,13 @@
+import jsonServer from "json-server";
+
+const server = jsonServer.create();
+const router = jsonServer.router("db.json");
+const middlewares = jsonServer.defaults();
+const port = 5000;
+
+server.use(middlewares);
+server.use(router);
+
+server.listen(port, () => {
+  console.log(`🚀 JSON Server is running at http://localhost:${port}`);
+});
